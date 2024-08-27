34a0947e42460898c7c216f943f7261fffa91db3
Volodymyr Masliy <thebestvova@gmail.com>
2024-08-27 19:00:11 +0200

    intellij-like dark theme

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -35,8 +35,28 @@ require('lazy').setup({
   {
     'rebelot/kanagawa.nvim',
     config = function()
-      vim.cmd.colorscheme 'kanagawa-dragon'
+      -- vim.cmd.colorscheme 'kanagawa-dragon'
     end,
+  },
+
+  {
+    'vermdeep/darcula_dark.nvim',
+    lazy = false,
+    priority = 1002,
+    config = function(_, opts)
+      -- enable colorscheme
+      vim.cmd 'set termguicolors'
+      vim.cmd [[colorscheme darcula_dark]]
+    end,
+  },
+
+  {
+    'rktjmp/lush.nvim',
+    lazy = false,
+    priority = 1000,
+    dependencies = {
+      'vermdeep/darcula_dark.nvim',
+    },
   },
 
   {
