cc428bcbd84c2a937d638f138dc9200063e4b580
Volodymyr Masliy <thebestvova@gmail.com>
2024-09-03 10:38:34 +0200

    repeatable indent in visual

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -14,44 +14,47 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnos
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
-vim.keymap.set("n", "\\\\", ":noh<CR>", { noremap = true })
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer.sh<CR>")
-vim.keymap.set("n", "==", vim.lsp.buf.format)
-vim.keymap.set("x", "<leader>p", [["_dP]])
-vim.keymap.set("n", "<leader>1", "1gt")
-vim.keymap.set("n", "<leader>2", "2gt")
-vim.keymap.set("n", "<leader>3", "3gt")
-vim.keymap.set("n", "<leader>4", "4gt")
-vim.keymap.set("n", "<leader>5", "5gt")
-vim.keymap.set("n", "<C-q>", ":qall<CR>");
+vim.keymap.set('n', '\\\\', ':noh<CR>', { noremap = true })
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+vim.keymap.set('n', '<C-f>', '<cmd>silent !tmux neww tmux-sessionizer.sh<CR>')
+vim.keymap.set('n', '==', vim.lsp.buf.format)
+vim.keymap.set('x', '<leader>p', [["_dP]])
+vim.keymap.set('n', '<leader>1', '1gt')
+vim.keymap.set('n', '<leader>2', '2gt')
+vim.keymap.set('n', '<leader>3', '3gt')
+vim.keymap.set('n', '<leader>4', '4gt')
+vim.keymap.set('n', '<leader>5', '5gt')
+vim.keymap.set('n', '<C-q>', ':qall<CR>')
 -- alternate file mapping on backspace
-vim.keymap.set("n", "<BS>", "<C-^>");
+vim.keymap.set('n', '<BS>', '<C-^>')
+
+-- repeatble indent in visual mode
+vim.keymap.set('x', '>', [[>gv]])
+vim.keymap.set('x', '<', [[<gv]])
 
 -- copilot
-vim.keymap.set("i", "<C-j>", "copilot#Next()", {expr=true, silent=true})
-vim.keymap.set("i", "<C-k>", "copilot#Previous()", {expr=true, silent=true})
+vim.keymap.set('i', '<C-j>', 'copilot#Next()', { expr = true, silent = true })
+vim.keymap.set('i', '<C-k>', 'copilot#Previous()', { expr = true, silent = true })
 
 -- Toogle file explorer
 -- vim.keymap.set("n", "<leader>e", "<cmd>Lexplore<CR>")
 -- vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
 -- vim.api.nvim_set_keymap("n", "<leader>e", ":NvimTreeToggle<cr>", {silent = true, noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>e", ":NvimTreeFindFile<cr>", { silent = true, noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeFindFile<cr>', { silent = true, noremap = true })
 
 -- Freed <C-l> in Netrw
 -- https://github.com/christoomey/vim-tmux-navigator/issues/189
 -- When in netrw, c-l is refreshing the file tree, but c-l is bind in neovim to move to the left pane
 -- The trick here is bind it some rnadom combo
-vim.keymap.set("n", "<leader><leader><leader><leader><leader><leader>l", "<Plug>NetrwRefresh")
+vim.keymap.set('n', '<leader><leader><leader><leader><leader><leader>l', '<Plug>NetrwRefresh')
 
+local api = require 'Comment.api'
 
-local api = require('Comment.api')
-
-vim.keymap.set("n", "<C-_>", api.toggle.linewise.current)
-vim.keymap.set("x", "<C-_>", '<Plug>(comment_toggle_linewise_visual)')
+vim.keymap.set('n', '<C-_>', api.toggle.linewise.current)
+vim.keymap.set('x', '<C-_>', '<Plug>(comment_toggle_linewise_visual)')
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
