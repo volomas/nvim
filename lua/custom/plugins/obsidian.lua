99214689a8aa1828ed8170411289cfd52284af7c
Volodymyr Masliy <thebestvova@gmail.com>
2024-12-05 10:12:57 +0100

    fix obsidian warning

--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -1,17 +1,18 @@
 return {
-  "epwalsh/obsidian.nvim",
-  version = "*",
+  'epwalsh/obsidian.nvim',
+  version = '*',
   lazy = true,
-  ft = "markdown",
+  ft = 'markdown',
   dependencies = {
     -- Required.
-    "nvim-lua/plenary.nvim",
+    'nvim-lua/plenary.nvim',
 
     -- see below for full list of optional dependencies 👇
   },
   config = function()
-    require("obsidian").setup {
-      dir = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/Volo",
+    require('obsidian').setup {
+      dir = '~/Library/Mobile Documents/iCloud~md~obsidian/Documents/Volo',
+      new_notes_location = 'current_dir',
       completion = {
         -- If using nvim-cmp, otherwise set to false
         nvim_cmp = true,
@@ -20,38 +21,37 @@ return {
         -- Where to put new notes created from completion. Valid options are
         --  * "current_dir" - put new notes in same directory as the current buffer.
         --  * "notes_subdir" - put new notes in the default notes subdirectory.
-        new_notes_location = "current_dir",
       },
       wiki_link_func = function(opts)
         if opts.id == nil then
-          return string.format("[[%s]]", opts.label)
+          return string.format('[[%s]]', opts.label)
         elseif opts.label ~= opts.id then
-          return string.format("[[%s|%s]]", opts.id, opts.label)
+          return string.format('[[%s|%s]]', opts.id, opts.label)
         else
-          return string.format("[[%s]]", opts.id)
+          return string.format('[[%s]]', opts.id)
         end
       end,
       mappings = {
         -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
-        ["gf"] = {
+        ['gf'] = {
           action = function()
-            return require("obsidian").util.gf_passthrough()
+            return require('obsidian').util.gf_passthrough()
           end,
           opts = { noremap = false, expr = true, buffer = true },
         },
       },
       follow_url_func = function(url)
         -- Open the URL in the default web browser.
-        vim.fn.jobstart({ "open", url }) -- Mac OS
+        vim.fn.jobstart { 'open', url } -- Mac OS
         -- vim.fn.jobstart({"xdg-open", url})  -- linux
       end,
-      sort_by = "modified",
+      sort_by = 'modified',
       sort_reversed = true,
 
       -- Optional, determines whether to open notes in a horizontal split, a vertical split,
       -- or replacing the current buffer (default)
       -- Accepted values are "current", "hsplit" and "vsplit"
-      open_notes_in = "current"
+      open_notes_in = 'current',
     }
-  end
+  end,
 }
