d68726169f876beccbf5983ba945f389e1d71658
Volodymyr Masliy <thebestvova@gmail.com>
2025-01-07 10:09:08 +0100

    fix ge gE

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -11,8 +11,8 @@ vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = tr
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', 'ge', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', 'gE', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', 'ge', vim.diagnostic.goto_next, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', 'gE', vim.diagnostic.goto_prev, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>E', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>dl', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
