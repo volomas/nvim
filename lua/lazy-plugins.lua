90fa8f8e0351144e24a9f91f8c88eaafb23adfbb
Volo <thebestvova@gmail.com>
2025-10-03 11:17:01 +0200

    git

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -141,6 +141,9 @@ require('lazy').setup({
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
   { import = 'custom.plugins' },
 }, {
+  git = {
+    url_format = 'git@github.com:%s.git', -- use SSH instead of HTTPS
+  },
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
     -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
