cddf4b8d68e9aa384411540f0e543815f38878d3
Volo <thebestvova@gmail.com>
2025-11-23 22:34:49 +0100

    remove neoscroll and supermaven

--- lua/custom/plugins/neoscroll.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'karb94/neoscroll.nvim',
-  config = function()
-    require('neoscroll').setup {
-      mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>', '<C-e>', '<C-y>' },
-    }
-  end,
-}
--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -178,6 +178,8 @@ return {
           --
           -- This may be unwanted, since they displace some of your code
           if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
+            -- Disable inlay hints by default
+            vim.lsp.inlay_hint.enable(false, { bufnr = event.buf })
             map('<leader>th', function()
               vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
             end, '[T]oggle Inlay [H]ints')
