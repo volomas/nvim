42d6087107d29b900fd944b90be4d0fd0ff536aa
Volo <thebestvova@gmail.com>
2025-12-11 18:57:14 +0100

    fix tmux sessionizer, formatting

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -19,11 +19,12 @@ return {
       formatters_by_ft = {
         json = { 'jq' },
         lua = { 'stylua' },
+        typescript = { 'prettierd', 'prettier', stop_after_first = true },
+        typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
       },
     },
   },
--- lua/options.lua
+++ lua/options.lua
@@ -25,6 +25,7 @@ vim.opt.colorcolumn = '100'
 
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
+vim.opt.expandtab = true
 vim.opt.smarttab = true
 
 -- Enable mouse mode
