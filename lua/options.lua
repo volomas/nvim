1f7121b2302c295ff58370fdad7a8916ee63414a
Volodymyr Masliy <thebestvova@gmail.com>
2026-02-18 14:19:19 +0100

    do not show tabs

--- lua/options.lua
+++ lua/options.lua
@@ -73,8 +73,8 @@ vim.o.splitbelow = true
 --  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
 --   See `:help lua-options`
 --   and `:help lua-options-guide`
-vim.o.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.o.list = false
+vim.opt.listchars = { trail = '·', nbsp = '␣' }
 
 -- Preview substitutions live, as you type!
 vim.o.inccommand = 'split'
