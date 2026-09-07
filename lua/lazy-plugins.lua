3bc7b4f8d701933b60f877c40008ce9393ea3f8c
Volodymyr Masliy <thebestvova@gmail.com>
2026-09-07 19:29:02 +0200

    gruvbox theme

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -29,6 +29,39 @@ require('lazy').setup({
 
   -- for markdown preview
   { 'ellisonleao/glow.nvim', config = true, cmd = 'Glow' },
+  {
+    'yousefhadder/markdown-plus.nvim',
+    ft = 'markdown',
+    opts = {},
+  },
+  {
+    'ellisonleao/gruvbox.nvim',
+    priority = 1000,
+    config = true,
+    opts = {
+      terminal_colors = true, -- add neovim terminal colors
+      undercurl = true,
+      underline = true,
+      bold = true,
+      italic = {
+        strings = true,
+        emphasis = true,
+        comments = true,
+        operators = false,
+        folds = true,
+      },
+      strikethrough = true,
+      invert_selection = false,
+      invert_signs = false,
+      invert_tabline = false,
+      inverse = true, -- invert background for search, diffs, statuslines and errors
+      contrast = 'hard', -- can be "hard", "soft" or empty string
+      palette_overrides = {},
+      overrides = {},
+      dim_inactive = false,
+      transparent_mode = false,
+    },
+  },
 
   -- themes
   {
